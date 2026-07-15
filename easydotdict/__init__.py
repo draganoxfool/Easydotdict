d9aa5c7ce8d25c70e2ded09a1381efca9089bcec
DRAGANO <draganogaming1@gmail.com>
2026-07-15 15:00:41 +0500

    bump version to 1.1.3

--- easydotdict/__init__.py
+++ easydotdict/__init__.py
@@ -1,7 +1,7 @@
 from .easydotdict import dotdict
 from .decorators import dotdictify, defaults, expect_schema, freeze
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 __all__ = [
     "dotdict",
     "dotdictify",
